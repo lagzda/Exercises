ee2569fef527394b2e798857e144d22aaa7fec2d
lagzda <ralfs.lagzda@gmail.com>
2015-12-21 02:16:34 +0200

    C#EX

---
+++ C#Ex/hello.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Hello{
+    static public void Main(string[] args){
+        Console.WriteLine("Introduce yourself: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello "+name+"!\nMy name is Computer");
+    }
+}
